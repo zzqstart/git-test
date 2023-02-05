0f216d6a37a961681048d120eb202250b8d52651
张哲琦 <1283453145@qq.com>
2023-02-05 10:42:31 +0800

    first commit

---
+++ src/main/java/com/zzq/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.zzq.git;
+
+/**
+ * @author zhangzheqi
+ * @create 2023-02-05 10:36
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
