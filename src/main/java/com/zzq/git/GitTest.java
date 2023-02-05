beb246d8cb5945f6de54cf7e34459b1c43b6844b
zzqstart <116236433+zzqstart@users.noreply.github.com>
2023-02-05 17:32:04 +0800

    pull commit

--- src/main/java/com/zzq/git/GitTest.java
+++ src/main/java/com/zzq/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("main test");
         System.out.println("hot-fix test");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
